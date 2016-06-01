0dae88a4fe68d8723502da2a28dfc040c4fa3849
Andrea <hybridcore23@gmail.com>
2016-06-01 19:08:53 +0200

    added mappingTest initial structure

---
+++ src/test/java/mappingTest/measurementGoalTest.java
@@ -0,0 +1,14 @@
+package mappingTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class measurementGoalTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/mappingTest/metricTest.java
@@ -0,0 +1,14 @@
+package mappingTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class metricTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
