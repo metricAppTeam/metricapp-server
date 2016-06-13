294cbc0ea5b9461edbc18507c55888275da2f39b
Andrea <hybridcore23@gmail.com>
2016-06-13 18:28:15 +0200

    added chageStateMetric to PUT of rest controller of Metrics

--- src/main/java/metricapp/service/controller/UserCRUDController.java
+++ src/main/java/metricapp/service/controller/UserCRUDController.java
@@ -1,13 +1,8 @@
 package metricapp.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import metricapp.dto.metric.MetricCrudDTO;
 import metricapp.service.spec.controller.UserCRUDControllerInterface;
 import metricapp.service.spec.repository.UserRepository;
 
