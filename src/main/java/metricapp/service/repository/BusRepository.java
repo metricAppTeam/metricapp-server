798b69aedad43f5ad095e78c05c43c13a97ec01f
Andrea <andrea.gennusa@gmail.com>
2016-06-15 01:53:51 +0200

    added @Javadoc to BusRepository

--- src/main/java/metricapp/service/repository/BusRepository.java
+++ src/main/java/metricapp/service/repository/BusRepository.java
@@ -50,7 +50,13 @@ public class BusRepository implements BusInterface {
     @Value("${bus.rollbackBus}")
     private String rollbackBus;
 
-
+    /**
+     * lowest level method to interact with bus, it requires only the type of request and the content. there's no "null check"
+     *
+     * @param content Array with 3 elements, it contains the phase number, the request (read, update, delete, rollback) and the content string
+     * @return the String content sended by the Bus. Tipically it is in JSON syntax, but it can contain Error String
+     * @throws BusException
+     */
     private String rawPost(ArrayList<String> content) throws BusException{
 
         BusRequestDTO requestDTO = new BusRequestDTO();
@@ -76,6 +82,15 @@ public class BusRepository implements BusInterface {
         return responseDTO.getBody().getContent();
     }
 
+
+    /**
+     * Simple function to create and fill the content array
+     *
+     * @param field0 identifies the phase number, typically it's fixed
+     * @param field1 identifies the kind of the request
+     * @param field2 identifies the request
+     * @return the filled array of elements
+     */
     private ArrayList<String> fillContent(String field0, String field1, String field2){
         ArrayList<String> content = new ArrayList<String>(3);
         content.add(0, field0);
@@ -86,22 +101,62 @@ public class BusRepository implements BusInterface {
     }
 
 
+    /**
+     * this method permits to interact with the bus through a read request.
+     * There's no null parameters check.
+     *
+     * @param pointerBusDTO
+     * @return the content of the response, it's a String in Json Syntax typically and needs to be converted
+     * @throws BusException if available append the previous exception to the last one
+     */
     public String read(PointerBusDTO pointerBusDTO) throws BusException{
         return rawPost(fillContent(phaseBus, readFromBus, pointerBusDTO.toJsonString()) );
     }
 
+    /**
+     * this method permits to interact with the bus through a update request.
+     * There's no null parameters check.
+     *
+     * @param pointerBusDTO
+     * @return the content of the response, it's a String in Json Syntax typically and needs to be converted
+     * @throws BusException if available append the previous exception to the last one
+     */
     public String update(PointerBusDTO pointerBusDTO) throws BusException{
         return rawPost(fillContent(phaseBus, updateToBus, pointerBusDTO.toJsonString()) );
     }
 
+    /**
+     * this method permits to interact with the bus through a create request.
+     * There's no null parameters check.
+     *
+     * @param pointerBusDTO
+     * @return the content of the response, it's a String in Json Syntax typically and needs to be converted
+     * @throws BusException if available append the previous exception to the last one
+     */
     public String create(PointerBusDTO pointerBusDTO) throws BusException{
         return rawPost(fillContent(phaseBus, createBus, pointerBusDTO.toJsonString()) );
     }
 
+    /**
+     * this method permits to interact with the bus through a rollback request.
+     * There's no null parameters check.
+     *
+     * @param pointerBusDTO
+     * @return the content of the response, it's a String in Json Syntax typically and needs to be converted
+     * @throws BusException if available append the previous exception to the last one
+     */
     public String rollback(PointerBusDTO pointerBusDTO) throws BusException{
         return rawPost(fillContent(phaseBus, rollbackBus, pointerBusDTO.toJsonString()) );
     }
 
+    /**
+     * this method permits to interact with the bus through a delete request.
+     * There's no null parameters check.
+     *
+     * @param pointerBusDTO
+     * @return the content of the response, it's a String.
+     * @throws BusException if available append the previous exception to the last one
+     */
     public String delete(PointerBusDTO pointerBusDTO) throws BusException{
         return rawPost(fillContent(phaseBus, deleteFromBus, pointerBusDTO.toJsonString()) );
     }
