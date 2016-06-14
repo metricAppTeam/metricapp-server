f7647c7c2c9db13c3e248734f939005c5a0aa840
Andrea <hybridcore23@gmail.com>
2016-06-14 18:36:48 +0200

    added externalelements dto

---
+++ src/main/java/metricapp/service/controller/ExternalElementsController.java
@@ -0,0 +1,12 @@
+package metricapp.service.controller;
+
+public class ExternalElementsController {
+	
+	/**
+	 * 
+	 * @param measurementGoalId
+	 * @return
+	 */
+	ExternalElementsDTO getMeasurementGoalExternalElements(String measurementGoalId)
+	
+}
