3041a74ade4a95b1cb0d277f8e6533167ff927de
alessandro <agil3b3ast@hotmail.it>
2016-06-14 11:21:38 +0200

    Removed .class in Entity enumeration

--- src/main/java/metricapp/entity/Entity.java
+++ src/main/java/metricapp/entity/Entity.java
@@ -1,19 +1,10 @@
 package metricapp.entity;
 
 public enum Entity {
-	MeasurementGoal (metricapp.entity.measurementGoal.MeasurementGoal.class),
-	Question (metricapp.entity.question.Question.class),
-	Metric (metricapp.entity.metric.Metric.class),
-	ContextFactor (ContextFactor.class),
-	Assumption (Assumption.class);
+	MeasurementGoal,
+	Question,
+	Metric,
+	ContextFactor,
+	Assumption;
 	
-	private Class<Element> associatedClass;
-	
-	private Entity(Class<?> c){
-		this.associatedClass=(Class<Element>) c;
-	}
-	
-	public Class<Element> getAssociatedClass(){
-		return this.associatedClass;
-	}
 }
