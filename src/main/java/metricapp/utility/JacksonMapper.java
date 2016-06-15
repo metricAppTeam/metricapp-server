c544f105c36afd7571d5ff6f19cd0890c5a75e77
Andrea <andrea.gennusa@gmail.com>
2016-06-15 02:59:41 +0200

    added toJson method to JacksonMapper

--- src/main/java/metricapp/utility/JacksonMapper.java
+++ src/main/java/metricapp/utility/JacksonMapper.java
@@ -2,21 +2,28 @@ package metricapp.utility;
 
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
  * This class is a utility, wired with Spring, that offers a lightweight converter for object to json string, and viceversa
  *
- * 
+ *
  * According to Jackson Documentation max performance of massive multithread use of mapper could be reached only
  * with the separated use of writer and reader.
  *
  * Jackson Documentation permits to use mapper with a single instance, it is ThreadSafe
+ *
+ * Example of use:
+ * jacksonMapper.toJson(myDTO) -> return String of Json
  */
+
+@Getter
 public class JacksonMapper {
 
     private ObjectMapper mapper;
@@ -37,6 +44,17 @@ public class JacksonMapper {
         mapper = new ObjectMapper();
         writer = mapper.writerWithDefaultPrettyPrinter();
         reader = mapper.reader();
+    }
+
+    /**
+     * this method is a simple wrapper of writeValueAsString of writer. 
+     *
+     * @param object that you want to convert
+     * @return String json formatted
+     * @throws JsonProcessingException
+     */
+    public String toJson(Object object) throws JsonProcessingException {
+       return this.getWriter().writeValueAsString(object);
     }
 
 
