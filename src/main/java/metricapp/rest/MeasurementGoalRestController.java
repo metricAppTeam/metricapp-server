bb2dce5385f029c45a7d8edfe0c7bfee968bfbf4
alessandro.fazio <agil3b3ast@hotmail.it>
2016-06-16 11:19:15 +0200

    Added CrossOrigin annotation to MeasurementGoalRestController

--- src/main/java/metricapp/rest/MeasurementGoalRestController.java
+++ src/main/java/metricapp/rest/MeasurementGoalRestController.java
@@ -3,6 +3,7 @@ package metricapp.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ import metricapp.exception.IllegalStateTransitionException;
 import metricapp.exception.NotFoundException;
 import metricapp.service.spec.controller.MeasurementGoalCRUDInterface;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/measurementgoal")
 public class MeasurementGoalRestController {
