9e741b704e81831649ce828de556c6c1c9939ec5
Andrea <hybridcore23@gmail.com>
2016-06-16 19:41:44 +0200

    added BusApprovedElementRepository

--- src/main/java/metricapp/service/spec/repository/BusInterface.java
+++ src/main/java/metricapp/service/spec/repository/BusInterface.java
@@ -3,21 +3,22 @@ package metricapp.service.spec.repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import metricapp.entity.external.PointerBus;
+import metricapp.entity.external.RichPointerBus;
 import metricapp.exception.BusException;
 
 public interface BusInterface {
 
-    public String read(PointerBus pointerBusDTO) throws BusException, JsonProcessingException;
+    public String read(PointerBus pointerBus) throws BusException, JsonProcessingException;
 
 
-    public String update(PointerBus pointerBusDTO) throws BusException, JsonProcessingException;
+    public String update(RichPointerBus pointerBus) throws BusException, JsonProcessingException;
 
 
-    public String create(PointerBus pointerBusDTO) throws BusException, JsonProcessingException;
+    public String create(RichPointerBus pointerBus) throws BusException, JsonProcessingException;
 
 
-    public String rollback(PointerBus pointerBusDTO) throws BusException, JsonProcessingException;
+    public String rollback(PointerBus pointerBus) throws BusException, JsonProcessingException;
 
-    public String delete(PointerBus pointerBusDTO) throws BusException, JsonProcessingException;
+    public String delete(PointerBus pointerBus) throws BusException, JsonProcessingException;
 
 }
