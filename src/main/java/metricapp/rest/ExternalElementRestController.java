b8f8b980606670a0b3350d74c6efd419baa028cb
alessandro.fazio <agil3b3ast@hotmail.it>
2016-07-08 18:11:27 +0200

    Added cross origin annotation

--- src/main/java/metricapp/rest/ExternalElementRestController.java
+++ src/main/java/metricapp/rest/ExternalElementRestController.java
@@ -8,6 +8,7 @@ import metricapp.service.spec.controller.ExternalElementsGetterInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 @RequestMapping(("/external"))
 public class ExternalElementRestController {
