09f0005f8fbf6daab467625dd0bfea0e298a12f5
Andrea <hybridcore23@gmail.com>
2016-07-09 23:03:57 +0200

    added smart constructors

--- src/main/java/metricapp/entity/external/KeyValue.java
+++ src/main/java/metricapp/entity/external/KeyValue.java
@@ -2,9 +2,11 @@ package metricapp.entity.external;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class KeyValue {
 
 	public String key;
