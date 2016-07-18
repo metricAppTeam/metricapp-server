b34deeb7fc51973cc0640702ad73392d18494aaf
Andrea <hybridcore23@gmail.com>
2016-07-18 13:15:25 +0200

    added update tsUpdate and tsCreate on creation and update

--- src/main/java/metricapp/entity/stakeholders/User.java
+++ src/main/java/metricapp/entity/stakeholders/User.java
@@ -6,8 +6,6 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
